148ff698699900555ba53ba9d7b611b220447d12
VallasPetr <vallaspetr@gmail.com>
2023-04-09 14:58:24 +0300

    change file README.md and change program Task01

--- TASK01/Program.cs
+++ TASK01/Program.cs
@@ -6,12 +6,27 @@
 // a = -3 b = 9 -> нет
 
 
+
+// Console.Write("Введите A: ");
+// string text1 = Console.ReadLine();
+// int number1 = Convert.ToInt32(text1);
+// Console.Write("Введите B: ");
+// string text2 = Console.ReadLine();
+// int number2 = Convert.ToInt32(text2);
+
+// if (number2 * number2 == number1)
+// {
+//     Console.WriteLine("A является квадратом B");
+// }
+// else
+// {
+//     Console.WriteLine("A не является квадратом B");
+// }
+
 Console.Write("Введите A: ");
-string text1 = Console.ReadLine();
-int number1 = Convert.ToInt32(text1);
+int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите B: ");
-string text2 = Console.ReadLine();
-int number2 = Convert.ToInt32(text2);
+int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number2 * number2 == number1)
 {
