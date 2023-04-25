9bab7d845b7e9da21d5925af2dd82ed0660d7094
VallasPetr <vallaspetr@gmail.com>
2023-04-25 21:43:46 +0300

    create task25, task27

--- TASK25/Program.cs
+++ TASK25/Program.cs
@@ -3,3 +3,23 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.Write("Введите число А = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA;
+if (numberB > 0)
+{
+    for (int i = 1; numberB > i; i++)
+    {
+        result = result*numberA;
+    }
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Ошибка! Число В должно быть натуральным");
+}
+
+
