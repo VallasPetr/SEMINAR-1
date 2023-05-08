c25cea0696b53aed06b5aab076d23753bd4d1d45
VallasPetr <vallaspetr@gmail.com>
2023-05-08 13:12:11 +0300

    create task36

---
+++ TASK36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив, заполненный случайными числами (длина массива 5 элементов). 
+// Найдите сумму элементов, стоящих на нечётных индексах (индексы с нуля).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[5];
+int count = array.Length;
+int odd=0;
+
+for (int i = 0; count > i; i++)
+{
+    int randomNumber = new Random().Next();
+    array[i] = randomNumber;
+    if(i%2==1) 
+    {
+        odd+=array[i];
+    }
+    Console.Write(array[i] + " ");
+    
+}
+Console.WriteLine();
+Console.Write(odd);
+
